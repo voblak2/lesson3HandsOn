f7dc7781c83e5b3bcac09d4acb4d40be271709e7
Vince Oblak <voblak2@gmail.com>
2020-10-08 14:35:33 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson3HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
