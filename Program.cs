69dacb1dd7a9904bde8ab0d6c529dd3b18edf4a6
Vince Oblak <voblak2@gmail.com>
2020-10-08 19:15:04 -0500

    looping through a string array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace lesson3HandsOn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] names = {"Emily", "Harry", "Rupert", "Clara", "Lily", "Michael"};
+            
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine("Have you seen " + names[i] + "?");
+                }
+            
         }
     }
 }
