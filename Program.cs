25d7102feff431831f7c3cb35ac1e74a409f64ec
Vince Oblak <voblak2@gmail.com>
2020-10-13 14:32:40 -0500

    final upload

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ namespace lesson3HandsOn
         {
             string[] names = {"Emily", "Harry", "Rupert", "Clara", "Lily", "Michael"};
             
+            //Printing the array in forward order
                 for (int i = 0; i < names.Length; i++)
                 {
                     Console.WriteLine("Have you seen " + names[i] + "?");
                 }
-            
+                
+            //Printing the array in reverse order
                 for (int i = names.Length -1; i >= 0; i--)
                 {
                     Console.WriteLine("Have you seen " + names[i] + "?");
